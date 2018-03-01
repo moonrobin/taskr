ff94acfa7400d89d6291ff1dde836ae09d5b4a47
Robin Lu <moonrobin@hotmail.com>
2018-03-01 18:43:45 +0800

    Added db initialization script

---
+++ db/init.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS users, tasks, bids;
+
+CREATE TABLE users
+(
+  username VARCHAR(100) PRIMARY KEY,
+  password VARCHAR(32) NOT NULL,
+  name VARCHAR(100) NOT NULL
+);
+
+CREATE UNIQUE INDEX upper_index
+  ON users (upper(username :: TEXT));
+
+CREATE TABLE tasks
+(
+  id SERIAL PRIMARY KEY,
+  acceptBid REAL DEFAULT 0,
+  acceptTime TIMESTAMP,
+  taskStartTime TIMESTAMP,
+  taskEndTime TIMESTAMP NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  description VARCHAR(999) NOT NULL,
+  requester VARCHAR(100) NOT NULL,
+  FOREIGN KEY (requester) REFERENCES users(username)
+);
+
+CREATE TABLE bids
+(
+  bid REAL,
+  task_id SERIAL PRIMARY KEY,
+  username VARCHAR(100),
+  FOREIGN KEY (task_id) REFERENCES tasks(id),
+  FOREIGN KEY (username) REFERENCES users(username)
+);
