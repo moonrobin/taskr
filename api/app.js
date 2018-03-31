8d54e0d0f279f6161eeb3e4662e645e76b405ed1
Monta Gao <mzgao@uwaterloo.ca>
2018-03-31 16:33:13 +0800

    Added cookie based login sessions - currently they store the username and save the session

--- api/app.js
+++ api/app.js
@@ -1,7 +1,25 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-app.use(cors({origin: 'http://localhost:1234'}));
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+
+app.use(cors({credentials: true, origin: 'http://localhost:1234'}));
+app.use(cookieSession({
+  name: 'session',
+  secret: 'secret'
+}));
+
+// Authentication and Authorization Middleware
+var auth = function(req, res, next) {
+  if (req.session && req.session.user){
+    return next();
+  } else {
+    // Forbidden access
+    console.log('forbidden access: ' + JSON.stringify(req.session));
+    return res.sendStatus(401);
+  }
+}
 
 const { Client } = require('pg')
 
@@ -17,9 +35,12 @@ client.connect();
 app.listen(3000);
 console.log('Now listening for requests on port 3000...');
 
+
+
+// Login Endpoint
 app.get('/login/:username/:password', function(req, res){
 
-  var text = `SELECT 1 FROM users WHERE username = $1 AND password = MD5($2)`;
+  var text = `SELECT username FROM users WHERE username = $1 AND password = MD5($2)`;
   values = [req.params.username, req.params.password];
 
   client.query(text, values, (err, result) => {
@@ -27,7 +48,8 @@ app.get('/login/:username/:password', function(req, res){
       console.log(err.stack);
     } else {
       if (result.rows[0]) {
-        console.log('User authenticated');
+        req.session.user = result.rows[0].username;  
+        console.log( 'Logged in as user ' + req.session.user );
         res.sendStatus(200);
       } else {
         console.log('No such user found');
@@ -37,6 +59,15 @@ app.get('/login/:username/:password', function(req, res){
   });
 });
 
+// Logout endpoint
+app.get('/logout', auth, function (req, res) {
+  console.log( 'Logged out as user: ' + req.session.user );
+  req.session = null;
+  res.sendStatus(200); // replace with static page/redirect
+
+});
+
+// Sign-up endpoint
 app.post('/createuser/:username/:password/:name', function(req, res){
 
   var text = `INSERT INTO users VALUES($1, MD5($2), $3)`;
@@ -51,4 +82,10 @@ app.post('/createuser/:username/:password/:name', function(req, res){
       res.sendStatus(201);
     }
   });
+});
+
+// for future content api calls
+app.get('/content', auth, function (req, res) {
+  // later we will read req.session.user and use it for our queries to DB
+  res.send('User will be able to see this only after they login');
 });
