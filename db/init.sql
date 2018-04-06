aa782c8f7fa27b5f1d8d51ad444a3d22e5d259c9
Monta Gao <mzgao@uwaterloo.ca>
2018-04-06 18:15:54 +0800

    Revert "Merge branch 'master' of https://github.com/moonrobin/taskr"
    This reverts commit 3bbb8c952ca2fe1033cfd50cbeea7f8b94a61c91, reversing
    changes made to 4239c98c3a1f3ea836329389d38d82597bf4ce29.

--- db/init.sql
+++ db/init.sql
@@ -27,40 +27,11 @@ CREATE TABLE tasks
 
 CREATE TABLE bids
 (
-  bid REAL NOT NULL,
-  task_id SERIAL,
+  bid REAL,
+  task_id SERIAL PRIMARY KEY,
   username VARCHAR(100),
   FOREIGN KEY (task_id) REFERENCES tasks(id)
     ON DELETE CASCADE,
   FOREIGN KEY (username) REFERENCES users(username)
-    ON DELETE CASCADE,
-  PRIMARY KEY(task_id, username)
+    ON DELETE CASCADE
 );
-
-DROP FUNCTION IF EXISTS update_tasks_table(); -- drop it
-CREATE OR REPLACE FUNCTION update_tasks_table() RETURNS TRIGGER AS $func$
-BEGIN
-  UPDATE tasks SET currentBid = NEW.bid
-  WHERE id = NEW.task_id AND (currentBid IS NULL OR currentBid > NEW.bid);
-  RETURN NEW;
-END;
-$func$ LANGUAGE plpgsql;
-
-DROP FUNCTION IF EXISTS update_tasks_table_on_delete(); -- drop it
-CREATE OR REPLACE FUNCTION update_tasks_table_on_delete() RETURNS TRIGGER AS $func2$
-BEGIN
-  UPDATE tasks SET currentBid = (SELECT MIN(bid) FROM bids WHERE task_id = OLD.task_id)
-  WHERE id = OLD.task_id;
-  RETURN OLD;
-END;
-$func2$ LANGUAGE plpgsql;
-
-CREATE TRIGGER bids_insert_or_update_trigger
-  AFTER INSERT OR UPDATE ON bids
-  FOR EACH ROW
-  EXECUTE PROCEDURE update_tasks_table();
-
-CREATE TRIGGER update_currentBid_in_bids_on_deletion
-  AFTER DELETE ON bids
-  FOR EACH ROW
-  EXECUTE PROCEDURE update_tasks_table_on_delete();
