2155f2404b1be3af452cd3425c64f02036ba1d09
Monta Gao <mzgao@uwaterloo.ca>
2018-04-07 17:01:00 +0800

    Linked task detail page with api calls

--- web/src/app/taskDisplayPage.js
+++ web/src/app/taskDisplayPage.js
@@ -25,22 +25,49 @@ class TaskDisplayPage extends React.Component{
     };
     this.bidTask = this.bidTask.bind(this);
     this.deleteTask = this.deleteTask.bind(this);
+    this.queryTask();
+  }
+
+  queryTask(){
+    var currentUrl = window.location.href;
+    console.log(currentUrl);
+    var matches = /task\/(\d+?)\b/.exec(currentUrl);
+    var taskId = matches[1];
+
+
+    var formedApiUrl = `http://localhost:3000/`+
+    `tasks?taskid=${taskId}`;
+
+    var data; 
+    var that = this;
+    fetch( formedApiUrl, { method: 'GET', credentials:'include'})
+    .then(function(res){
+        return res.json();
+    }).then( function(resjson){
+        data = resjson;
+        console.log( data );
+        that.setState({
+          data: data[0]
+        });
+    });   
+
   }
 
   bidTask() {
+    var createBidApiUrl = `http://localhost:3000`;
 
   }
 
   deleteTask() {
-
+    var deleteTaskApiUrl = `http://localhost:3000`;
   }
 
   render() {
     var rows = [];
     rows.push();
-    for(var key in mockData) {
+    for(var key in this.state.data) {
       if(key != "id" && mockData[key] != null) {
-        var row = <DetailItem attr={d[key]} value={mockData[key]}/>;
+        var row = <DetailItem attr={d[key]} value={this.state.data[key]}/>;
         rows.push(row);
       }
     }
