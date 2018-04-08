ad93a64e920aee06708b28fbecde8be1b01c3f4b
Robin Lu <moonrobin@hotmail.com>
2018-04-08 19:07:12 +0800

    Added ER diagram, report figures

--- db/init.sql
+++ db/init.sql
@@ -5,20 +5,20 @@ CREATE TYPE STATE AS ENUM ('bidding','awarded', 'unfulfilled', 'complete');
 CREATE TABLE users
 (
   username VARCHAR(100) PRIMARY KEY,
-  password VARCHAR(32) NOT NULL,
+  password VARCHAR(32) NOT NULL, -- MD5 hash of the password
   name VARCHAR(100) NOT NULL,
-  admin boolean DEFAULT FALSE NOT NULL
+  admin boolean DEFAULT FALSE NOT NULL -- indicates if the user is an admin
 );
 
 CREATE UNIQUE INDEX upper_index
-  ON users (upper(username :: TEXT));
+  ON users (upper(username :: TEXT)); -- Enforces all usernames are case insensitively unique
 
 CREATE TABLE tasks
 (
   id SERIAL PRIMARY KEY, -- Auto incrementing unique ID for each task
   startBid REAL NOT NULL, -- Price which bidding starts
   currentBid REAL, -- Current lowest bid on task (Null indicates no bid was placed)
-  acceptBid REAL DEFAULT 0 NOT NULL, -- If the bid drops to this price, then the task is auto awarded
+  acceptBid REAL DEFAULT 0 NOT NULL, -- If the bid drops to this price, then the task is awarded
   acceptTime TIMESTAMP, -- Optional time to end the bid
   taskStartTime TIMESTAMP DEFAULT '-infinity' NOT NULL,
   taskEndTime TIMESTAMP NOT NULL,
